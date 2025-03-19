90bd0250ef315ee9db6dea411cac3c89916e8d1e
maxfrysmastarn36 <tyr.lundstrom@elev.ga.ntig.se>
2025-03-19 12:03:49 +0100

    Fixade ett problem

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const lista = document.querySelector(".product-list")
     const cartpage = document.querySelector(".cart-content")
+    const cartIcon = document.querySelector(".shopping-cart")
     cartpage.style.display = "none";
 
     cartIcon.addEventListener("click", () => {
